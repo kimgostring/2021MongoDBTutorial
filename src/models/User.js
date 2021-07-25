59728e485545ead9f61109b10e99dc77b6d79d77
kimgahyeon <kimgostring@naver.com>
2021-07-25 17:46:52 +0900

    Node.js로 MongoDB 다루기
    Express에서 오류 처리하기

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+// 구조 분해 할당 이용
+const { Schema, model } = require('mongoose');
 
 // User라는 데이터는 어떤 key/value를 가지는지, 필수인지 등의 정보 
 // DB 업데이트 전, 이 정보를 통해 mongoose가 형식에 맞는지 확인해 줌
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
     username: { type: String, required: true },
     name: {
         first: { type: String, required: true },
@@ -13,5 +14,5 @@ const UserSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // MongoDB compass _MONGOSH에서의 db처럼 User 이용할 수 있음 (DB의 CRUD 작업 가능)
-const User = mongoose.model('user', UserSchema);
+const User = model('user', UserSchema);
 module.exports = { User };
--- src/server.js
+++ src/server.js
@@ -22,9 +22,23 @@ const server = async () => {
         });
 
         app.post('/users', async (req, res) => {
-            const user = new User(req.body); // 유저 문서 인스턴스를 mongoose로 생성
-            await user.save(); // mongoose에서 제공하는 함수를 통해 해당 문서를 DB에 저장
-            res.send({ success: true, user });
+            try {
+                // 구조 분해 할당
+                let { username, name } = req.body;
+                    // 아래와 동일한 구문
+                    // let username = req.body.username
+                    // let name = req.body.name
+                // required: true인 username, name.first가 제대로 들어왔는지 확인
+                if (!username) return res.status(400).send({ err: 'username is required. '});
+                if (!name || !name.first) return res.status(400).send({ err: 'first name is required.' });
+
+                const user = new User(req.body); // 유저 문서 인스턴스를 mongoose로 생성
+                await user.save(); // mongoose에서 제공하는 함수를 통해 해당 문서를 DB에 저장
+                res.send({ success: true, user });  
+            } catch(err) {
+                return res.status(500).send({ err : err.message }); // server error
+                // err.message, 오류 발생 스택 부분은 빼고 오류 메세지만 보낼 수 있음
+            }
         })
 
         app.listen(port, () => {
