fb778b74119273bbbe6c49e283f4a13e0be6241e
kimgahyeon <kimgostring@naver.com>
2021-07-26 23:28:03 +0900

    관계된 데이터 관리하기
    User와 관계된 Blog Model

---
+++ src/models/Blog.js
@@ -0,0 +1,17 @@
+const { Schema, model, Types } = require("mongoose");
+
+const BlogSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    // islive가 false면 임시저장, true면 게시 완료 (공개)
+    islive: { type: Boolean, require: true, default: false },
+    // 블로그 - 유저 관계 생김, 한 유저는 여러 블로그 사용 가능
+    // 어떤 모델과 관계를 가지는지, ref를 통해 컬렉션명(단수)를 mongoose에게 알려줌
+    user: { type: Types.ObjectId, required: true, ref: "user" },
+  },
+  { timestamps: true }
+);
+
+const Blog = model("blog", BlogSchema);
+module.exports = { Blog };
