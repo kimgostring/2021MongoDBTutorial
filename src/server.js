ff5e8177705559bc8e1a8c5eeb3dc83050b808b5
kimgahyeon <kimgostring@naver.com>
2021-07-26 23:08:10 +0900

    Node.js로 MongoDB 다루기
    save로 document 수정하기

--- src/server.js
+++ src/server.js
@@ -1,29 +1,38 @@
-const express = require('express'); 
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const config = require('./config');
-const { userRouter } = require('./routes/userRoute'); 
+const mongoose = require("mongoose");
+const config = require("./config");
+const { userRouter } = require("./routes/userRoute");
 
 const port = 3000;
 
 const server = async () => {
-    try {
-        // DB가 먼저 연결된 뒤 요청 받아야 함 (mongoose 연결 완료된 뒤 포트 수신 해야 함)
-        await mongoose.connect(config.MONGO_URL, 
-            // 설정 객체 (옵션), mongoose 연결 시마다 뜨는 DeprecationWarning 제거해줌
-            { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
-        console.log('MongoDB connected');
-        app.use(express.json());
+  try {
+    // DB가 먼저 연결된 뒤 요청 받아야 함 (mongoose 연결 완료된 뒤 포트 수신 해야 함)
+    await mongoose.connect(
+      config.MONGO_URL,
+      // 설정 객체 (옵션), mongoose 연결 시마다 뜨는 DeprecationWarning 제거해줌
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }
+    );
+    console.log("MongoDB connected");
+    // mongoose.set("debug", true); // mongoose debug mode
 
-        // 라우터
-        app.use('/users', userRouter);
+    app.use(express.json());
 
-        app.listen(port, () => {
-            console.log(`server listening on port ${port}`);
-        });
-    } catch (err) {
-        console.log(err);
-    }
+    // 라우터
+    app.use("/users", userRouter);
+
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 server();
