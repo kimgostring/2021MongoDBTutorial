af8c0514988b73562119c0bdaff91a7d236c32c9
kimgahyeon <kimgostring@naver.com>
2021-07-31 17:26:33 +0900

    관계된 데이터 관리하기
    POST /comment

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -1,21 +1,44 @@
 const { Router } = require("express");
 const { Comment } = require("../models/Comment");
+const { Blog } = require("../models/Blog");
+const { User } = require("../models/User");
+const { isValidObjectId } = require("mongoose");
 
 // blogRoute.js에서 설정한 엔드포인트 /:blogId의 값 불러올 수 있게 해주는 설정
 const commentRouter = Router({ mergeParams: true });
 
-/*  
-    comment의 경로?
-    - 위의 두 개와 달리 독립적으로 호출될 일 X, 하위 개념
-    - 특정 블로그의 댓글 등을 불러오게 될 것
-*/
+// comment의 경로?
+// - 위의 두 개와 달리 독립적으로 호출될 일 X, 하위 개념
+// - 특정 블로그의 댓글 등을 불러오게 될 것
 
-// 등록 API
-commentRouter.post("/:commentId", async (req, res) => {
-  res.send(req.params);
+// 댓글 등록 API
+commentRouter.post("/", async (req, res) => {
+  try {
+    const { blogId } = req.params;
+    if (!isValidObjectId(blogId))
+      return res.status(400).send({ err: "blog id is invalid." });
+
+    const { content, userId } = req.body;
+    if (!isValidObjectId(userId))
+      return res.status(400).send({ err: "user id is invalid." });
+    if (typeof content !== "string")
+      return res.status(400).send({ err: "content is required." });
+
+    const blog = await Blog.findById(blogId);
+    const user = await User.findById(userId);
+    if (!blog || !user)
+      return res.status(400).send({ err: "blog or user does not exist." });
+    if (!blog.islive)
+      return res.status(400).send({ err: "blog is not available." });
+
+    const comment = new Comment({ content, user, blog });
+    res.send({ success: true, comment });
+  } catch (err) {
+    return res.status(500).send({ err: err.message });
+  }
 });
 
-// 조회 API
+// 댓글 조회 API
 commentRouter.get("/");
 
 module.exports = { commentRouter };
