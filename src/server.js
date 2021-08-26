4e29de1dabb0f6b2ccac4314606afa35e0cd70b9
KimGahyeon <kimgostring@naver.com>
2021-08-26 16:10:12 +0900

    AWS에 Node.js 백엔드 배포하기
    .env로 환경변수 설정

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const config = require("./config");
 // 경로에 폴더명만 존재할 경우, 해당 폴더의 index.js 찾아 불러오게 됨
 const { userRouter, blogRouter } = require("./routes");
 // 테스트용 데이터 생성
-const { generateFakeData } = require("../faker2");
+// const { generateFakeData } = require("../faker2");
 const port = 3000;
 
 const server = async () => {
   try {
+    const { MONGO_URL } = process.env;
+    if (!MONGO_URL) throw new Error("MONGO_URL is required. ");
+
     // DB가 먼저 연결된 뒤 요청 받아야 함 (mongoose 연결 완료된 뒤 포트 수신 해야 함)
     await mongoose.connect(
-      config.MONGO_URL,
+      MONGO_URL,
       // 설정 객체 (옵션), mongoose 연결 시마다 뜨는 DeprecationWarning 제거해줌
       {
         useNewUrlParser: true,
